af9968f61df42bbd895082460490b0a7d38a1dc8
buikevin96 <buikevin96@gmail.com>
2019-01-10 22:29:27 -0500

    upcasting and downcasting

---
+++ 29upcastingDowncasting/src/App.java
@@ -0,0 +1,46 @@
+class Machine {
+	public void start() {
+		System.out.println("Machine started.");
+	}
+}
+
+class Camera extends Machine{
+	public void start() {
+		System.out.println("Camera started.");
+	}
+	
+	public void snap() {
+		System.out.println("Photo taken");
+	}
+	
+}
+
+public class App {
+	public static void main(String[] args) {
+		
+		// Object
+		Machine machine1 = new Machine();
+		Camera camera1 = new Camera();
+		
+		machine1.start();
+		camera1.start();
+		camera1.snap();
+		
+		//Upcasting
+		Machine machine2 = camera1;
+		machine2.start();
+		
+		//Downcasting
+		Machine machine3 = new Camera();
+		Camera camera2 = (Camera)machine3;
+		camera2.snap();
+		camera2.start();
+		
+		// Doesn't work; runtime error
+		Machine machine4 = new Machine();
+		//Camera camera3 = (Camera)machine4;
+		//camera3.start();
+		//camera3.snap();
+		
+	}
+}
