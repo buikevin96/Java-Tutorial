489628c410a928fdf80659008e55197467db0dd9
buikevin96 <buikevin96@gmail.com>
2019-01-13 12:38:14 -0500

    Abstract Classes

---
+++ 38abstractClasses/src/Camera.java
@@ -0,0 +1,23 @@
+
+public class Camera extends Machine{
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		System.out.println("Starting Camera");
+	}
+
+	@Override
+	public void doStuff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
---
+++ 38abstractClasses/src/Car.java
@@ -0,0 +1,23 @@
+
+public class Car extends Machine{
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		System.out.println("Starting Car");
+	}
+
+	@Override
+	public void doStuff() {
+		// TODO Auto-generated method stub
+		System.out.println("Do stuff in car");
+	}
+
+	@Override
+	public void shutDown() {
+		// TODO Auto-generated method stub
+		System.out.println("Shut down car");
+	}
+	
+
+}
